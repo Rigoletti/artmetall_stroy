2b6b13d75f85a778f5d6c4fba9071bc3122c929c
Rigoletti <tupoyjena@gmail.com>
2025-06-10 12:41:36 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Portfolio from '@/components/portfolio/Portfolio';
-import Contact from '@/components/contact/Contact';
-import Services from '@/components/services/Services';
-import HomeSection from '@/components/home/HomeSection';
-import PostProject from '@/components/post_project/PostProject';
 
 const App = () => {
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path="/" element={<HomeSection />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/post_project" element={<PostProject />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <div style={{ color: 'white', background: 'black', padding: '2rem' }}>
+      Минимальное приложение работает!
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+console.log('React app started'); 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
